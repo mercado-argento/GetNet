1c62e76b3a0f2f9c5353a7637d1ef10e807939e1
Mercado Argento <166949066+mercado-argento@users.noreply.github.com>
2025-12-11 23:19:35 -0300

    Refactor Magento services for PHP 8.2 compatibility

--- Model/RestoreStock.php
+++ Model/RestoreStock.php
@@ -6,98 +6,95 @@
  */
 namespace GetnetArg\Payments\Model;
 
-
 class RestoreStock extends \Magento\Framework\View\Element\Template
 {
-    
-    protected $logger;
-    
-    protected $moduleManager;
-    
-    protected $resourceConnection;
-    
-    public $_objectManager;
 
-    
+    protected $logger;
+
+    protected $moduleManager;
+
+    protected $resourceConnection;
+
+    private \Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite $stockIdForCurrentWebsite;
+
     /**
-     * 
-     * 
+     *
+     *
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Psr\Log\LoggerInterface $logger,
-        \Magento\Framework\ObjectManagerInterface $_objectManager,
         \Magento\Framework\Module\Manager $moduleManager,
         \Magento\Framework\App\ResourceConnection $resourceConnection,
-        \Magento\Sales\Model\Order $order,
+        \Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite $stockIdForCurrentWebsite,
         array $data = []
     ) {
         parent::__construct($context, $data);
 
-        $this->_objectManager     = $_objectManager;
         $this->moduleManager      = $moduleManager;
         $this->resourceConnection = $resourceConnection;
         $this->logger = $logger;
-        $this->order = $order;
+        $this->stockIdForCurrentWebsite = $stockIdForCurrentWebsite;
 
     }
 
 
     /**
-     * 
-     * 
+     *
+     *
      */
     public function execute($order){
-        
-	     try{
+
+             try{
                 //if inventory is enabled
                 if (!$this->isInventoryEnabled())
                     return;
-        
+
                 $this->logger->debug('isInventoryEnabled --> restore cart');
-        
+
                 $connection = $this->resourceConnection->getConnection();
-                $stockId    = $this->_objectManager->get('\Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite');
-                
+                $stockId    = $this->stockIdForCurrentWebsite;
+
 
                 foreach ($order->getAllItems() as $item) {
                     $product = $item->getProduct();
-                    
+
                     $quantity = $item->getQtyOrdered();
-                    
+
                     $this->logger->debug('Quantity --> ' .$quantity);
-                    
+
                     $metadata = [
                         'event_type'          => "back_item_qty",
                         "object_type"         => "legacy_stock_management_api",
                         "object_id"           => "",
                         "object_increment_id" => $order->getIncrementId()
                     ];
-        
+
                     $query = "INSERT INTO inventory_reservation (stock_id, sku, quantity, metadata)
-                        VALUES (".$stockId->execute().", '".$product->getSku()."', ".$quantity.", '".json_encode($metadata)."');"; 
-        
+                        VALUES (".$stockId->execute().", '".$product->getSku()."', ".$quantity.", '".json_encode($metadata)."');
+";
+
                     //Insert data in db
                     $connection->query($query);
-        
+
                     $this->logger->debug('inserto-----');
                 }
-            
-                
+
+
             } catch (\Exception $e) {
                     $this->logger->debug(' exception --> ' . $e);
             }
 
     }
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
    /**
      * Check if Magento inventory feature is enabled.
-     * 
+     *
      */
     public function isInventoryEnabled()
     {
@@ -116,5 +113,5 @@ class RestoreStock extends \Magento\Framework\View\Element\Template
 
         return true;
     }
-        
+
 }
